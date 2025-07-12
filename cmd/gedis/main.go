83f749580b3f4f250ac8eac07b2b3f0355c3ad4b
tientn01 <tientn@coin98.finance>
2025-07-12 16:30:34 +0700

    feat: add accept connection separately

--- cmd/gedis/main.go
+++ cmd/gedis/main.go
@@ -2,20 +2,14 @@ package main
 
 import (
 	"fmt"
-	"net"
-	"os"
+
+	"github.com/TrNgTien/gedis.git/internal/server"
 )
 
 
 func main(){
   fmt.Println("Gedis is started at port 6379.....")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-
-  defer l.Close()
+  server.RunAsyncServer()
 }
+
